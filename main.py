0e868abf34c36ade4591fea46a5726083cc473e1
tyrovirtuosso <tyrovirtuosso@proton.me>
2023-04-09 12:58:42 +0530

    Made the code a bit neater!

--- main.py
+++ main.py
@@ -4,6 +4,23 @@ import configparser
 from termcolor import colored
 
 
+class ConfigHandler:
+    def __init__(self, config_file):
+        self.config = configparser.ConfigParser()
+        self.config.read(config_file)
+
+    def get_storage_handler(self):
+        storage_choice = int(self.config.get('Settings', 'storage_choice'))
+        if storage_choice == 1:
+            return FolderStorage()
+        elif storage_choice == 2:
+            return SQLiteStorage()
+        elif storage_choice == 3:
+            return HDFSStorage()
+        else:
+            raise ValueError("Invalid storage choice")
+
+
 def create_fetcher(category, storage_handler):
     if category == 'crypto':
         return CoinGeckoFetcher(storage_handler=storage_handler)
@@ -13,38 +30,25 @@ def create_fetcher(category, storage_handler):
         raise ValueError(f"Unknown category: {category}")
 
 
-def read_config(config_file):
-    config = configparser.ConfigParser()
-    config.read(config_file)
-
-    storage_choice = int(config.get('Settings', 'storage_choice'))
-    if storage_choice == 1:
-        storage_handler = FolderStorage()
-    elif storage_choice == 2:
-        storage_handler = SQLiteStorage()
-    elif storage_choice == 3:
-        storage_handler = HDFSStorage()
-    else:
-        raise ValueError("Invalid storage choice")
-
-    return storage_handler
-
-
-def main():
-    storage_handler = read_config('config.ini')
-
-    symbols = {
-        'crypto': ['bitcoin'],
-        'stock': ['AAPL']
-    }
-
+def update_symbols(symbols, storage_handler):
     for category, symbol_list in symbols.items():        
         fetcher = create_fetcher(category, storage_handler)
         for symbol in symbol_list:
             print("\n")
             print(f"Updating {category} {colored(symbol.upper(), 'green', attrs=['bold'])}")
-            updated_data = fetcher.update_data(symbol) 
-            
+            updated_data = fetcher.update_data(symbol)
+
+
+def main():
+    config_handler = ConfigHandler('config.ini')
+    storage_handler = config_handler.get_storage_handler()
+
+    symbols = {
+        'crypto': ['matic-network', 'chainlink'],
+        'stock': ['ARKK']
+    }
+
+    update_symbols(symbols, storage_handler)
 
 
 if __name__ == "__main__":
